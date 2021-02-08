ac985850830706e24276ec4eca40beca7a1aa18b
rhnm <rhnm@pm.me>
2021-02-08 19:43:11 +0330

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
+
+
+### SETTING THE STARSHIP PROMPT ###
+starship init fish | source
