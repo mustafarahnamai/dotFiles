d18eeed6661112faed1370ff37bd1bf32d87388b
rhnm <you@example.com>
2021-06-09 19:41:23 +0430

    theme change for starship and fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,9 @@
 ### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
-set fish_color_normal brcyan
+set fish_color_normal grey
 set fish_color_autosuggestion '#7d7d7d'
-set fish_color_command brcyan
+set fish_color_command grey
 set fish_color_error '#ff6c6b'
-set fish_color_param brcyan
+set fish_color_param grey
 
 
 ### SETTING THE STARSHIP PROMPT ###
